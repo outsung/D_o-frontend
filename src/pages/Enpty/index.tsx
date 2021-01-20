48a5c8d1c5b1b17233dac93c892afb77894cc4b6
outsung <insung9546@gmail.com>
2021-01-21 00:52:49 +0900

    v.0

--- src/pages/Enpty/index.tsx
+++ src/pages/Enpty/index.tsx
@@ -4,6 +4,7 @@ import { EnptyPage, ErrorCodeText, HmmText } from './style';
 
 const Empty = () => (
   <EnptyPage>
+    <ErrorCodeText>v.0</ErrorCodeText>
     <ErrorCodeText>404</ErrorCodeText>
     <HmmText>Hmm..</HmmText>
   </EnptyPage>
